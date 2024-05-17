c807fbcba0ee8db22c7ba001f02c3276fddae284
Max Maier <mmaier5900@student.pps.net>
2024-05-17 11:37:37 -0700

    stuf

--- src/main/java/org/work/work/mixin/testMixin.java
+++ src/main/java/org/work/work/mixin/testMixin.java
@@ -1,17 +1,30 @@
 package org.work.work.mixin;
 
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.network.message.MessageHandler;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SignedMessage;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.gui.hud.ChatHudLine;
+
 
 @Mixin(MessageHandler.class)
 
 public class testMixin {
-    @Inject(at = @At("HEAD"), method = "onChatMessage()V")
-    private void init(CallbackInfo info) {
-        System.out.println("This line is printed by an example mod mixin!");
+    @Inject(at = @At("HEAD"), method = "onChatMessage()V", cancellable = true)
+
+    private void onChatMessage(SignedMessage message, GameProfile sender, MessageType.Parameters params, CallbackInfo info) {
+        SignedMessage thing = new SignedMessage(message.link(), message.signature(), message.signedBody(), message.unsignedContent(), message.filterMask());
+        assert message.unsignedContent() != null;
+        System.out.println("[=] " + message.getContent().getString());
+
+        info.cancel();
+
     }
 }
