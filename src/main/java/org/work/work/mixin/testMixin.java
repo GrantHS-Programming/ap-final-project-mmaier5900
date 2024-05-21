238a4289ba0c03bf60ea614437fa73549fb51d17
287316 <alt061629@gmail.com>
2024-05-20 23:38:04 -0700

    hfx

--- src/main/java/org/work/work/mixin/testMixin.java
+++ src/main/java/org/work/work/mixin/testMixin.java
@@ -1,6 +1,7 @@
 package org.work.work.mixin;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.network.message.MessageHandler;
@@ -22,9 +23,25 @@ public class testMixin {
     private void onChatMessage(SignedMessage message, GameProfile sender, MessageType.Parameters params, CallbackInfo info) {
         SignedMessage thing = new SignedMessage(message.link(), message.signature(), message.signedBody(), message.unsignedContent(), message.filterMask());
         assert message.unsignedContent() != null;
+
+        //message.getContent().replace("what");
         System.out.println("[=] " + message.getContent().getString());
+
+
+        if (MinecraftClient.getInstance().player != null) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("Hello, friend." + message.getContent().getString()));
+        }
 
         info.cancel();
 
     }
+
+
+    private static void displayCustomMessage() {
+        // Send your custom message to the client chat.
+        if (MinecraftClient.getInstance().player != null) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("adfadf"));
+        }
+    }
+
 }
