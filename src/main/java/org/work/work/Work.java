91afb42376ef01f6e04c9ad6454e3fdd724fd346
Max Maier <mmaier5900@student.pps.net>
2024-06-11 10:40:53 -0700

    testbuilfd

--- src/main/java/org/work/work/Work.java
+++ src/main/java/org/work/work/Work.java
@@ -1,32 +1,36 @@
 package org.work.work;
 
 import net.fabricmc.api.ModInitializer;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.kyrptonaught.customportalapi;
+
 import java.time.ZonedDateTime;
 
 public class Work implements ModInitializer {
 
     // * Runs the mod initializer.
-CustomPortalBuilder.beginPortal()
-        .frameBlock(Blocks.DIAMOND_BLOCK)
-        .lightWithItem(Items.ENDER_EYE)
-        .destDimID(new Identifier("nowhere"))
-            .tintColor(45,65,101)
-        .registerPortal();
+
     public static final testItem CUSTOM_ITEM = new testItem(new Item.Settings());
     public static final firstBlock DOT_BLOCK = new firstBlock(AbstractBlock.Settings.create().strength(4.0f).requiresTool());
 
     @Override
     public void onInitialize() {
+        CustomPortalBuilder.beginPortal()
+                .frameBlock(Blocks.GOLD_BLOCK)
+                .lightWithItem(Items.ENDER_EYE)
+                //.lightWithFluid(Fluids.WATER)
+                .destDimID(new Identifier("tutorial", "nowhere"))
+                .tintColor(234, 183, 8)
+                .registerPortal();
+
+
         System.out.println(ZonedDateTime.now());
         Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
         Registry.register(Registries.BLOCK, new Identifier("tutorial", "first_block"), DOT_BLOCK);
